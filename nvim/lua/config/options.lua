d61d133a342038928d5b2f4093cf6c4b5b380fdc
all_done <danilchenko.aleksey94@gmail.com>
2024-04-25 15:13:38 +0700

    update

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,13 @@
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
+
+
+
+vim.o.number = true
+vim.o.spelllang = "ru_ru,en_us"
+vim.o.linebreak = true
+vim.g.autoformat = false
+
+vim.cmd([[set colorcolumn=80]])
+
---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,49 @@
+return {
+{
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+      vim.fn.sign_define("DiagnosticSignError",
+        {text = " ", texthl = "DiagnosticSignError"})
+      vim.fn.sign_define("DiagnosticSignWarn",
+        {text = " ", texthl = "DiagnosticSignWarn"})
+      vim.fn.sign_define("DiagnosticSignInfo",
+        {text = " ", texthl = "DiagnosticSignInfo"})
+      vim.fn.sign_define("DiagnosticSignHint",
+        {text = "󰌵", texthl = "DiagnosticSignHint"})
+
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      window = {
+        width = 30,
+      },
+      buffers = {
+        follow_current_file = {
+		enabled = true,
+	},
+      },
+      filesystem = {
+        follow_current_file = {
+		enabled = true,
+	},
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            "node_modules"
+          },
+          never_show = {
+            ".DS_Store",
+            "thumbs.db"
+          },
+        },
+      },
+    })
+  end
+},
+}
